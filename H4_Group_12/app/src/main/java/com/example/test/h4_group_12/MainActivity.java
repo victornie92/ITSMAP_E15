f2f98aff6090a33f43c19623905cf238af29699e
201205925 <201205925@post.au.dk>
2015-09-24 10:44:34 +0200

    Broadcast til Activity
    Broadcaster fra Service til Activity hver 10. sekund, og updater text i
    Activity

--- H4_Group_12/app/src/main/java/com/example/test/h4_group_12/MainActivity.java
+++ H4_Group_12/app/src/main/java/com/example/test/h4_group_12/MainActivity.java
@@ -1,22 +1,24 @@
 package com.example.test.h4_group_12;
 
-import android.app.Service;
+import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.ServiceConnection;
 import android.os.IBinder;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Context context = this;
     Button subButton;
+    TextView temperatureValue,windSpeedValue;
     WeatherService weatherService;
     boolean serviceBound = false;
 
@@ -26,13 +28,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         subButton = (Button)findViewById(R.id.subBtn);
+        temperatureValue = (TextView)findViewById(R.id.tempValue);
+        windSpeedValue = (TextView)findViewById(R.id.windSpeed);
 
         subButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(serviceBound){
-                    weatherService.message();
-                    //Toast.makeText(MainActivity.this, "Potato", Toast.LENGTH_SHORT).show();
+                    if(subButton.getText().toString().equals("Subscribe")) {
+                        LocalBroadcastManager.getInstance(context).registerReceiver(
+                                weatherUpdateReceiver, new IntentFilter("WeatherUpdate"));
+                        subButton.setText("Unsubscribe");
+                    }
+                    else if(subButton.getText().toString().equals("Unsubscribe")) {
+                        LocalBroadcastManager.getInstance(context).unregisterReceiver(
+                                weatherUpdateReceiver);
+                        subButton.setText("Subscribe");
+                    }
                 }
             }
         });
@@ -50,7 +62,6 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
         if(serviceBound){
             unbindService(serviceConnection);
-
         }
     }
 
@@ -60,12 +71,19 @@ public class MainActivity extends AppCompatActivity {
             WeatherService.LocalWeatherBinder binder = (WeatherService.LocalWeatherBinder) service;
             weatherService = binder.getService();
             serviceBound = true;
-
         }
 
         @Override
         public void onServiceDisconnected(ComponentName name) {
             serviceBound = false;
+        }
+    };
+
+    private BroadcastReceiver weatherUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            temperatureValue.setText(intent.getStringExtra("Temperature"));
+            windSpeedValue.setText(intent.getStringExtra("WindSpeed"));
         }
     };
 }
--- H4_Group_12/app/src/main/java/com/example/test/h4_group_12/WeatherService.java
+++ H4_Group_12/app/src/main/java/com/example/test/h4_group_12/WeatherService.java
@@ -4,15 +4,23 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
+import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
 import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.logging.LogRecord;
 
 /**
  * Created by Teardrop Ducky on 23/09/2015.
  */
 public class WeatherService extends Service {
 
+    final Handler weatherHandler = new Handler();
     private final IBinder binder = new LocalWeatherBinder();
+    int i = 0;
 
     public class LocalWeatherBinder extends Binder {
         WeatherService getService(){
@@ -28,10 +36,22 @@ public class WeatherService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Toast.makeText(WeatherService.this, "fap fap fap", Toast.LENGTH_SHORT).show();
-    }
 
-    public void message(){
-        Toast.makeText(WeatherService.this, "shit works", Toast.LENGTH_SHORT).show();
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                weatherHandler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent update = new Intent("WeatherUpdate");
+                        update.putExtra("Temperature", String.valueOf(i++));
+                        update.putExtra("WindSpeed",String.valueOf(i++));
+                        LocalBroadcastManager.getInstance(WeatherService.this).sendBroadcast(update);
+                    }
+                });
+            }
+        };
+        timer.schedule(timerTask,0,10000);
     }
 }
